eb8f6a93235827bb522f1d49377916aa2f2dd985
francinaclermont <francinaclermont@gmail.com>
2025-01-20 18:32:04 +0100

    Table_metricas_test.R

--- codi/999_Outputs_finals/Table_metricas_test.R
+++ codi/999_Outputs_finals/Table_metricas_test.R
@@ -1,16 +1,17 @@
 library(tidyverse)
-# Lista todos los archivos en el directorio
+# Llista d'arxius en el directori
 archivos_paths <- list.files(path = 'programacio/outputs/Experiments/') %>%  as.list()
 
+# mètriques de tots els experiments del dataset test
 
 archivos_paths %>% 
   map(~read_rds(paste0('programacio/outputs/Experiments/',..1,'/Taula_Metriques_test.rds') )) %>%  
   bind_rows() %>% 
   arrange(Experiment) %>% 
   mutate(order = case_when(
-    Experiment %in% c("10_SMOTE_n5", "11_DownSMOTE_2_n2", "12_DownSMOTE_2_n5") ~ 2, # Asignar prioridad alta (2)
-    TRUE ~ 1 # Prioridad baja para el resto (1)
+    Experiment %in% c("10_SMOTE_n5", "11_DownSMOTE_2_n2", "12_DownSMOTE_2_n5") ~ 2,
+    TRUE ~ 1 
   )) %>% 
-  arrange(order, Experiment) %>%  # Ordenar primero por 'order', luego por 'Experiment'
+  arrange(order, Experiment) %>%  
   select(-order, -Avg_number_obs) %>%  
   writexl::write_xlsx(path='programacio/outputs/Finals/Experiments_Metriques_test.xlsx')
