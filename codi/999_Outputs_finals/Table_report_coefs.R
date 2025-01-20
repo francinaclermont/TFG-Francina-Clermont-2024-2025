39f6e1bf784217d6c86149c6053bd3cb793b647d
francinaclermont <francinaclermont@gmail.com>
2025-01-20 18:35:01 +0100

    Table_report_coefs.R

--- codi/999_Outputs_finals/Table_report_coefs.R
+++ codi/999_Outputs_finals/Table_report_coefs.R
@@ -1,19 +1,19 @@
 library(tidyverse)
-# Lista todos los archivos en el directorio
+# Llista d'arxius en el directori
 archivos_paths <- list.files(path = 'programacio/outputs/Experiments/') %>%  as.list()
 
-
+# creem taula amb els coeficients dels models generats en els experiments
 
 archivos_paths %>% 
   map(~read_rds(paste0('programacio/outputs/Experiments/', ..1, '/Tibble_Coefs_models.rds'))) %>%  
   bind_rows() %>% 
   arrange(experimento) %>% 
   mutate(order = case_when(
-    experimento %in% c("10_SMOTE_n5", "11_DownSMOTE_2_n2", "12_DownSMOTE_2_n5") ~ 2, # Asignar prioridad alta (2)
-    TRUE ~ 1 # Prioridad baja para el resto (1)
+    experimento %in% c("10_SMOTE_n5", "11_DownSMOTE_2_n2", "12_DownSMOTE_2_n5") ~ 2,
+    TRUE ~ 1
   )) %>% 
-  arrange(order, experimento) %>%  # Ordenar primero por 'order', luego por 'experimento'
-  mutate(experimento = factor(experimento, levels = unique(experimento))) %>% # Convertir a factor con niveles en el orden actual
+  arrange(order, experimento) %>%  # ordenem 'order',i 'experimento'
+  mutate(experimento = factor(experimento, levels = unique(experimento))) %>% convertim a factor
   select(-order, -statistic) %>%
   
   group_by(experimento, term) %>% 
@@ -22,7 +22,7 @@ archivos_paths %>%
   pivot_wider(names_from = 'term', values_from = 'value') %>%
   as.data.frame() %>% 
   
-  # Usar `format()` para evitar notación científica y limitar a 2 decimales
+  # `format()` evita notació científica i limita a 2 decimals
   mutate(across(where(is.numeric), ~ format(., digits = 2, nsmall = 0, scientific = FALSE))) %>% 
   arrange(metric) %>% 
   writexl::write_xlsx(.,'programacio/outputs/Finals/Reporting_coefs.xlsx')
@@ -34,19 +34,18 @@ archivos_paths %>%
   bind_rows() %>% 
   arrange(experimento) %>% 
   mutate(order = case_when(
-    experimento %in% c("10_SMOTE_n5", "11_DownSMOTE_2_n2", "12_DownSMOTE_2_n5") ~ 2, # Asignar prioridad alta (2)
-    TRUE ~ 1 # Prioridad baja para el resto (1)
+    experimento %in% c("10_SMOTE_n5", "11_DownSMOTE_2_n2", "12_DownSMOTE_2_n5") ~ 2, 
+    TRUE ~ 1 
   )) %>% 
-  arrange(order, experimento) %>%  # Ordenar primero por 'order', luego por 'experimento'
-  mutate(experimento = factor(experimento, levels = unique(experimento))) %>% # Convertir a factor con niveles en el orden actual
+  arrange(order, experimento) %>% 
+  mutate(experimento = factor(experimento, levels = unique(experimento))) %>% 
   select(-order, -statistic) %>%
   
   group_by(experimento, term) %>% 
   summarize(across(estimate:p.value, ~mean(., na.rm = TRUE))) %>%  print(n=500) %>% 
   writexl::write_xlsx('programacio/outputs/Finals/Reporting_coefs_2.xlsx')
 
-  
-  
+
   
   
   
