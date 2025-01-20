cb4e6da0edd19c212ab81ae3d3c8e55557eec279
francinaclermont <francinaclermont@gmail.com>
2025-01-20 18:32:54 +0100

    Table_metricas_training.R

--- codi/999_Outputs_finals/Table_metricas_training.R
+++ codi/999_Outputs_finals/Table_metricas_training.R
@@ -1,16 +1,17 @@
 library(tidyverse)
-# Lista todos los archivos en el directorio
+# Llista d'arxius del directori
 archivos_paths <- list.files(path = 'programacio/outputs/Experiments/') %>%  as.list()
 
+# Creem taula amb les mètriques dels experiments de dataset train
 
 archivos_paths %>% 
   map(~read_rds(paste0('programacio/outputs/Experiments/',..1,'/Taula_Metriques_train.rds') )) %>%  
   bind_rows() %>% 
   arrange(Experiment) %>% 
   mutate(order = case_when(
-    Experiment %in% c("10_SMOTE_n5", "11_DownSMOTE_2_n2", "12_DownSMOTE_2_n5") ~ 2, # Asignar prioridad alta (2)
-    TRUE ~ 1 # Prioridad baja para el resto (1)
+    Experiment %in% c("10_SMOTE_n5", "11_DownSMOTE_2_n2", "12_DownSMOTE_2_n5") ~ 2, 
+    TRUE ~ 1 
   )) %>% 
-  arrange(order, Experiment) %>%  # Ordenar primero por 'order', luego por 'Experiment'
-  select(-order) %>% # Eliminar la columna auxiliar de orden 
+  arrange(order, Experiment) %>% 
+  select(-order) %>% 
   writexl::write_xlsx(path='programacio/outputs/Finals/Experiments_Metriques_train.xlsx')
